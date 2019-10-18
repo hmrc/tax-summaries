e22256a5311946a4596202044144a0d7c2d8b422
“brendanstraughan” <brendan.straughan@digital.hmrc.gov.uk>
2019-10-18 09:00:44 +0100

    DDCNLS-6912 Added in *get* and *some* to unit tests

--- test/services/GovSpendServiceTest.scala
+++ test/services/GovSpendServiceTest.scala
@@ -27,122 +27,122 @@ class GovSpendServiceTest extends UnitSpec with MockitoSugar with ScalaFutures {
     "return correct amounts for 2014" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2014)
 
-      result("Welfare") shouldBe 24.52
-      result("Health") shouldBe 18.87
-      result("Education") shouldBe 13.15
-      result("StatePensions") shouldBe 12.12
-      result("NationalDebtInterest") shouldBe 7.0
-      result("Defence") shouldBe 5.31
-      result("CriminalJustice") shouldBe 4.4
-      result("Transport") shouldBe 2.95
-      result("BusinessAndIndustry") shouldBe 2.74
-      result("GovernmentAdministration") shouldBe 2.05
-      result("Culture") shouldBe 1.69
-      result("Environment") shouldBe 1.66
-      result("HousingAndUtilities") shouldBe 1.64
-      result("OverseasAid") shouldBe 1.15
-      result("UkContributionToEuBudget") shouldBe 0.75
+      result.get("Welfare") shouldBe Some(24.52)
+      result.get("Health") shouldBe Some(18.87)
+      result.get("Education") shouldBe Some(13.15)
+      result.get("StatePensions") shouldBe Some(12.12)
+      result.get("NationalDebtInterest") shouldBe Some(7.0)
+      result.get("Defence") shouldBe Some(5.31)
+      result.get("CriminalJustice") shouldBe Some(4.4)
+      result.get("Transport") shouldBe Some(2.95)
+      result.get("BusinessAndIndustry") shouldBe Some(2.74)
+      result.get("GovernmentAdministration") shouldBe Some(2.05)
+      result.get("Culture") shouldBe Some(1.69)
+      result.get("Environment") shouldBe Some(1.66)
+      result.get("HousingAndUtilities") shouldBe Some(1.64)
+      result.get("OverseasAid") shouldBe Some(1.15)
+      result.get("UkContributionToEuBudget") shouldBe Some(0.75)
     }
 
     "return correct amounts for 2015" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2015)
 
-      result("Welfare") shouldBe 25.3
-      result("Health") shouldBe 19.9
-      result("StatePensions") shouldBe 12.8
-      result("Education") shouldBe 12.5
-      result("Defence") shouldBe 5.4
-      result("NationalDebtInterest") shouldBe 5.0
-      result("PublicOrderAndSafety") shouldBe 4.4
-      result("Transport") shouldBe 3.0
-      result("BusinessAndIndustry") shouldBe 2.7
-      result("GovernmentAdministration") shouldBe 2.0
-      result("Culture") shouldBe 1.8
-      result("Environment") shouldBe 1.7
-      result("HousingAndUtilities") shouldBe 1.6
-      result("OverseasAid") shouldBe 1.3
-      result("UkContributionToEuBudget") shouldBe 0.6
+      result.get("Welfare") shouldBe Some(25.3)
+      result.get("Health") shouldBe Some(19.9)
+      result.get("StatePensions") shouldBe Some(12.8)
+      result.get("Education") shouldBe Some(12.5)
+      result.get("Defence") shouldBe Some(5.4)
+      result.get("NationalDebtInterest") shouldBe Some(5.0)
+      result.get("PublicOrderAndSafety") shouldBe Some(4.4)
+      result.get("Transport") shouldBe Some(3.0)
+      result.get("BusinessAndIndustry") shouldBe Some(2.7)
+      result.get("GovernmentAdministration") shouldBe Some(2.0)
+      result.get("Culture") shouldBe Some(1.8)
+      result.get("Environment") shouldBe Some(1.7)
+      result.get("HousingAndUtilities") shouldBe Some(1.6)
+      result.get("OverseasAid") shouldBe Some(1.3)
+      result.get("UkContributionToEuBudget") shouldBe Some(0.6)
     }
 
 
     "return correct amounts for 2016" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2016)
 
-      result("Welfare") shouldBe 25.00
-      result("Health") shouldBe 19.90
-      result("StatePensions") shouldBe 12.80
-      result("Education") shouldBe 12.00
-      result("Defence") shouldBe 5.20
-      result("NationalDebtInterest") shouldBe 5.30
-      result("PublicOrderAndSafety") shouldBe 4.30
-      result("Transport") shouldBe 4.00
-      result("BusinessAndIndustry") shouldBe 2.40
-      result("GovernmentAdministration") shouldBe 2.00
-      result("Culture") shouldBe 1.60
-      result("Environment") shouldBe 1.70
-      result("HousingAndUtilities") shouldBe 1.40
-      result("OverseasAid") shouldBe 1.20
-      result("UkContributionToEuBudget") shouldBe 1.10
+      result.get("Welfare") shouldBe Some(25.00)
+      result.get("Health") shouldBe Some(19.90)
+      result.get("StatePensions") shouldBe Some(12.80)
+      result.get("Education") shouldBe Some(12.00)
+      result.get("Defence") shouldBe Some(5.20)
+      result.get("NationalDebtInterest") shouldBe Some(5.30)
+      result.get("PublicOrderAndSafety") shouldBe Some(4.30)
+      result.get("Transport") shouldBe Some(4.00)
+      result.get("BusinessAndIndustry") shouldBe Some(2.40)
+      result.get("GovernmentAdministration") shouldBe Some(2.00)
+      result.get("Culture") shouldBe Some(1.60)
+      result.get("Environment") shouldBe Some(1.70)
+      result.get("HousingAndUtilities") shouldBe Some(1.40)
+      result.get("OverseasAid") shouldBe Some(1.20)
+      result.get("UkContributionToEuBudget") shouldBe Some(1.10)
     }
 
     "return correct amounts for 2017" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2017)
 
-      result("Welfare") shouldBe 24.30
-      result("Health") shouldBe 20.30
-      result("StatePensions") shouldBe 12.90
-      result("Education") shouldBe 12.30
-      result("Defence") shouldBe 5.20
-      result("NationalDebtInterest") shouldBe 5.50
-      result("PublicOrderAndSafety") shouldBe 4.20
-      result("Transport") shouldBe 4.20
-      result("BusinessAndIndustry") shouldBe 2.50
-      result("GovernmentAdministration") shouldBe 2.10
-      result("Culture") shouldBe 1.60
-      result("Environment") shouldBe 1.60
-      result("HousingAndUtilities") shouldBe 1.50
-      result("OverseasAid") shouldBe 1.10
-      result("UkContributionToEuBudget") shouldBe 0.70
+      result.get("Welfare") shouldBe Some(24.30)
+      result.get("Health") shouldBe Some(20.30)
+      result.get("StatePensions") shouldBe Some(12.90)
+      result.get("Education") shouldBe Some(12.30)
+      result.get("Defence") shouldBe Some(5.20)
+      result.get("NationalDebtInterest") shouldBe Some(5.50)
+      result.get("PublicOrderAndSafety") shouldBe Some(4.20)
+      result.get("Transport") shouldBe Some(4.20)
+      result.get("BusinessAndIndustry") shouldBe Some(2.50)
+      result.get("GovernmentAdministration") shouldBe Some(2.10)
+      result.get("Culture") shouldBe Some(1.60)
+      result.get("Environment") shouldBe Some(1.60)
+      result.get("HousingAndUtilities") shouldBe Some(1.50)
+      result.get("OverseasAid") shouldBe Some(1.10)
+      result.get("UkContributionToEuBudget") shouldBe Some(0.70)
     }
 
     "return correct amounts for 2018" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2018)
 
-      result("Welfare") shouldBe 23.80
-      result("Health") shouldBe 19.90
-      result("StatePensions") shouldBe 12.80
-      result("Education") shouldBe 12.00
-      result("Defence") shouldBe 5.30
-      result("NationalDebtInterest") shouldBe 6.10
-      result("PublicOrderAndSafety") shouldBe 4.30
-      result("Transport") shouldBe 4.30
-      result("BusinessAndIndustry") shouldBe 2.90
-      result("GovernmentAdministration") shouldBe 2.10
-      result("Culture") shouldBe 1.60
-      result("Environment") shouldBe 1.60
-      result("HousingAndUtilities") shouldBe 1.60
-      result("OverseasAid") shouldBe 1.20
-      result("UkContributionToEuBudget") shouldBe 0.70
+      result.get("Welfare") shouldBe Some(23.80)
+      result.get("Health") shouldBe Some(19.90)
+      result.get("StatePensions") shouldBe Some(12.80)
+      result.get("Education") shouldBe Some(12.00)
+      result.get("Defence") shouldBe Some(5.30)
+      result.get("NationalDebtInterest") shouldBe Some(6.10)
+      result.get("PublicOrderAndSafety") shouldBe Some(4.30)
+      result.get("Transport") shouldBe Some(4.30)
+      result.get("BusinessAndIndustry") shouldBe Some(2.90)
+      result.get("GovernmentAdministration") shouldBe Some(2.10)
+      result.get("Culture") shouldBe Some(1.60)
+      result.get("Environment") shouldBe Some(1.60)
+      result.get("HousingAndUtilities") shouldBe Some(1.60)
+      result.get("OverseasAid") shouldBe Some(1.20)
+      result.get("UkContributionToEuBudget") shouldBe Some(0.70)
     }
 
     "return correct amounts for 2019" in {
       val result: Map[String, BigDecimal] = GovSpendService.govSpending(2019)
 
-      result("Welfare") shouldBe 23.50
-      result("Health") shouldBe 20.20
-      result("StatePensions") shouldBe 12.80
-      result("Education") shouldBe 11.80
-      result("Defence") shouldBe 5.30
-      result("NationalDebtInterest") shouldBe 5.10
-      result("Transport") shouldBe 4.30
-      result("PublicOrderAndSafety") shouldBe 4.30
-      result("BusinessAndIndustry") shouldBe 3.6
-      result("GovernmentAdministration") shouldBe 2.10
-      result("HousingAndUtilities") shouldBe 1.60
-      result("Environment") shouldBe 1.50
-      result("Culture") shouldBe 1.50
-      result("OverseasAid") shouldBe 1.20
-      result("UkContributionToEuBudget") shouldBe 1.00
+      result.get("Welfare") shouldBe Some(23.50)
+      result.get("Health") shouldBe Some(20.20)
+      result.get("StatePensions") shouldBe Some(12.80)
+      result.get("Education") shouldBe Some(11.80)
+      result.get("Defence") shouldBe Some(5.30)
+      result.get("NationalDebtInterest") shouldBe Some(5.10)
+      result.get("Transport") shouldBe Some(4.30)
+      result.get("PublicOrderAndSafety") shouldBe Some(4.30)
+      result.get("BusinessAndIndustry") shouldBe Some(3.6)
+      result.get("GovernmentAdministration") shouldBe Some(2.10)
+      result.get("HousingAndUtilities") shouldBe Some(1.60)
+      result.get("Environment") shouldBe Some(1.50)
+      result.get("Culture") shouldBe Some(1.50)
+      result.get("OverseasAid") shouldBe Some(1.20)
+      result.get("UkContributionToEuBudget") shouldBe Some(1.00)
     }
   }
 
