775aa02db043e00b89f4b16139e918ccaabaf32f
Andrew Arkless <AndrewArkless@users.noreply.github.com>
2019-10-28 09:24:33 +0000

    DDCNLS-5055 Config  tests added

--- test/config/ApplicationConfigSpec.scala
+++ test/config/ApplicationConfigSpec.scala
@@ -18,7 +18,7 @@ package config
 
 import org.scalatest.TestData
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
-import play.api.{Application, Mode}
+import play.api.Application
 import play.api.inject.guice.GuiceApplicationBuilder
 import uk.gov.hmrc.play.test.UnitSpec
 
@@ -26,16 +26,36 @@ class ApplicationConfigSpec extends UnitSpec with GuiceOneAppPerTest {
   //abstract class WithSetup(additionalConfiguration: Map[String, _] = Map.empty)
 
   override def newAppForTest(testData: TestData): Application = {
-    val builder = new GuiceApplicationBuilder().in(Mode.Test)
 
-    def appWithConfig(config: Map[String, _]): Application = builder.configure(config).build()
+    def appWithConfig(config: Map[String, Any]): Application = {
+      new GuiceApplicationBuilder().configure(Map("taxRates" -> "")).configure(config).build()
+    }
 
-    appWithConfig(Map("taxRates.default.whitelist" -> Seq("TaxField1","TaxField2"),"taxRates.2017.whitelist" -> Seq("TaxField3")))
+    val taxRates = Map("taxRates.default.whitelist" -> Seq("TaxField1", "TaxField2"), "taxRates.2017.whitelist" -> Seq("TaxField3"))
+
+    val percentageRates = Map("taxRates.default.percentages" -> Map("percentageRate1" -> 10, "percentageRate2" -> 20, "percentageRate3" -> 30), "taxRates.2017.percentages" -> Map("percentageRate2" -> 60))
+
+    appWithConfig(taxRates ++ percentageRates)
   }
 
   "calling rate TaxFields" should {
-    "return list taxFields" in {
-      ApplicationConfig.taxFields(2017) shouldBe Seq("TaxField1","TaxField2","TaxField3")
+    "return default taxFields when no overriding year" in {
+      ApplicationConfig.taxFields(2016) shouldBe Seq("TaxField1", "TaxField2")
+    }
+
+    "return default taxFields with extra fields  for specific year" in {
+      ApplicationConfig.taxFields(2017) shouldBe Seq("TaxField1", "TaxField2", "TaxField3")
+    }
+
+  }
+
+  "calling ratePercentages" should {
+    "return default percentage rates when no overriding year" in {
+      ApplicationConfig.ratePercentages(2016) shouldBe Map("percentageRate1" -> 10, "percentageRate2" -> 20, "percentageRate3" -> 30)
+    }
+
+    "return default percentage but override where later year uprates" in {
+      ApplicationConfig.ratePercentages(2017) shouldBe Map("percentageRate1" -> 10, "percentageRate2" -> 60, "percentageRate3" -> 30)
     }
   }
 }
