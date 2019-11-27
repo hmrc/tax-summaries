f832f24a481b23ab18ab578d257298ec4ada1595
“brendanstraughan” <brendan.straughan@digital.hmrc.gov.uk>
2019-11-27 10:57:58 +0000

    DDCNLS-6912 - Review changes made

--- app/transformers/ATSRawDataTransformer.scala
+++ app/transformers/ATSRawDataTransformer.scala
@@ -159,14 +159,14 @@ case class ATSRawDataTransformer(
     )
 
   private def createCapitalGainsTaxRates: Map[RateKey, ApiRate] =
-    Map(
+    Map[RateKey, Rate](
       CapitalGainsEntrepreneur -> taxRate.cgEntrepreneursRate,
       CapitalGainsOrdinary     -> taxRate.cgOrdinaryRate,
       CapitalGainsUpper        -> taxRate.cgUpperRate,
       TotalCapitalGains        -> calculations.totalCgTaxLiabilityAsPercentage,
       InterestLower            -> taxRate.individualsForResidentialPropertyAndCarriedInterestLowerRate,
       InterestHigher           -> taxRate.individualsForResidentialPropertyAndCarriedInterestHigherRate
-    ).mapValues(_.apiValue).map(apiValue => apiValue)
+    ).mapValues(_.apiValue)
 
   private def createSummaryPageRates: Map[RateKey, ApiRate] =
     Map(
@@ -175,7 +175,7 @@ case class ATSRawDataTransformer(
     )
 
   private def createTotalIncomeTaxPageRates: Map[RateKey, ApiRate] =
-    Map(
+    Map[RateKey, Rate](
       Savings          -> taxRate.startingRateForSavingsRate,
       IncomeBasic      -> taxRate.basicRateIncomeTaxRate,
       IncomeHigher     -> taxRate.higherRateIncomeTaxRate,
@@ -183,5 +183,5 @@ case class ATSRawDataTransformer(
       Ordinary         -> taxRate.dividendsOrdinaryRate,
       Upper            -> taxRate.dividendUpperRateRate,
       Additional       -> taxRate.dividendAdditionalRate
-    ).mapValues(_.apiValue).map(apiValue => apiValue)
+    ).mapValues(_.apiValue)
 }
