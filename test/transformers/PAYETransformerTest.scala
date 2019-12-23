a24cd03578d61f2ab5c759590ee460cd3dc79e39
beckfordp <beckfordp@btinternet.com>
2019-12-23 16:53:06 +0000

    ATS-109 Employer NIC Amount

--- test/transformers/PAYETransformerTest.scala
+++ test/transformers/PAYETransformerTest.scala
@@ -160,6 +160,10 @@ class PAYETransformerTest extends UnitSpec with PAYETransformer with GuiceOneApp
           |        "amount" : 200,
           |        "currency" : "GBP"
           |      },
+          |      "employer_nic_amount" : {
+          |        "amount" : 100,
+          |        "currency" : "GBP"
+          |      },
           |      "nics_and_tax_rate" : {
           |        "amount" : 25,
           |        "currency" : "GBP"
