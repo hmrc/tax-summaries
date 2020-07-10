db8dec191911d69cffedd4f45e20edd523152556
Steven Truong <45595406+oc-ste@users.noreply.github.com>
2020-07-10 10:45:32 +0100

    DDCNL-1980: Removed unused json4 libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,10 +9,8 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc"       %% "bootstrap-play-25"      % "5.3.0",
     "uk.gov.hmrc"       %% "domain"                 % "5.6.0-play-25",
-    "org.json4s"        %% "json4s-jackson"         % "3.6.7",
     "uk.gov.hmrc"       %% "time"                   % "3.6.0",
     "com.github.fge"     % "json-schema-validator"  % "2.2.6",
-    "org.json4s"        %% "json4s-native"          % "3.5.5",
     "uk.gov.hmrc"       %% "auth-client"            % "2.31.0-play-25",
     "org.reactivemongo" %% "play2-reactivemongo"    % "0.18.8-play25"
   )
@@ -22,9 +20,7 @@ object AppDependencies {
       "com.typesafe.play"       %% "play-test"              % PlayVersion.current,
       "org.scalatestplus.play"  %% "scalatestplus-play"     % "2.0.1",
       "org.jsoup"                % "jsoup"                  % "1.12.1",
-      "org.json4s"              %% "json4s-jackson"         % "3.6.7",
       "com.github.fge"           % "json-schema-validator"  % "2.2.6",
-      "org.json4s"              %% "json4s-native"          % "3.5.5",
       "org.mockito"              % "mockito-all"            % "1.10.19",
       "org.scalacheck"          %% "scalacheck"             % "1.14.3",
       "com.github.tomakehurst"   % "wiremock"               % "2.26.0"
