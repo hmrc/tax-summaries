1413200f75ba51e8c93d9fe966cb836e069a57a4
Christian-QA <credfernbus@gmail.com>
2022-07-26 15:45:32 +0100

    Re-upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"                %% "domain"                    % s"6.1.0-$playVersion",
     "uk.gov.hmrc"                %% "time"                      % "3.25.0",
     "com.github.fge"             % "json-schema-validator"      % "2.2.6",
-    "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"  % "0.67.0",
+    "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"  % "0.68.0",
     "com.typesafe.scala-logging" %% "scala-logging"             % "3.9.2",
     "org.typelevel"              %% "cats-core"                 % "2.6.1"
   )
@@ -31,7 +31,7 @@ object AppDependencies {
     "org.pegdown"            %  "pegdown"                   % "1.6.0",
     "com.vladsch.flexmark"   % "flexmark-all"               % "0.35.10",
     "uk.gov.hmrc"            %% "tax-year"                  % "1.6.0",
-    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playVersion"  % "0.67.0"
+    "uk.gov.hmrc.mongo"       %% s"hmrc-mongo-test-$playVersion"  % "0.68.0"
   ).map(_ % "test,it")
 
   val all: Seq[ModuleID] = compile ++ test
