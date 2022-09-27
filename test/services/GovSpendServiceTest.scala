7bfe569d40c93cf50bc37900bddf308b88ec2c1a
Christian-QA <credfernbus@gmail.com>
2022-09-27 14:44:31 +0100

    DDCNL-6416: Added 2022 GovSpend tests

--- test/services/GovSpendServiceTest.scala
+++ test/services/GovSpendServiceTest.scala
@@ -42,5 +42,26 @@ class GovSpendServiceTest extends BaseSpec {
       result.get(OverseasAid) mustBe Some(1.20)
       result.get(UkContributionToEuBudget) mustBe Some(1.10)
     }
+
+    "return correct amounts for 2022" in {
+      val result: Map[GoodsAndServices, Double] =
+        new GovSpendService(applicationConfig).govSpending(2022)
+
+      result.get(Health) mustBe Some(22.80)
+      result.get(Welfare) mustBe Some(20.40)
+      result.get(StatePensions) mustBe Some(11.00)
+      result.get(Education) mustBe Some(10.50)
+      result.get(NationalDebtInterest) mustBe Some(7.60)
+      result.get(BusinessAndIndustry) mustBe Some(5.40)
+      result.get(Defence) mustBe Some(5.10)
+      result.get(Transport) mustBe Some(4.70)
+      result.get(PublicOrderAndSafety) mustBe Some(4.40)
+      result.get(GovernmentAdministration) mustBe Some(2.00)
+      result.get(HousingAndUtilities) mustBe Some(1.60)
+      result.get(Environment) mustBe Some(1.50)
+      result.get(Culture) mustBe Some(1.30)
+      result.get(OverseasAid) mustBe Some(0.90)
+      result.get(OutstandingPaymentsToTheEU) mustBe Some(0.70)
+    }
   }
 }
