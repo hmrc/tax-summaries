80d95ca9528630bf67900a0d14679d61d54c2b8a
SandeepUmredkar <sandeep.umredkar@digital.hmrc.gov.uk>
2023-08-30 00:15:01 +0100

    DDCNL-7625

--- test/transformers/ATSCalculationsTest.scala
+++ test/transformers/ATSCalculationsTest.scala
@@ -250,13 +250,14 @@ class ATSCalculationsTest extends BaseSpec with ScalaCheckPropertyChecks with Do
 
     "includePensionIncomeForRate returns StatePensionGross when percentages match" in {
 
-      forAll { (rate: Double, totalVal: Double) =>
+      forAll { (rateI: Int, totalValI: Int) =>
+        val rate = Math.abs(rateI).toDouble / 100.0
+        val totalVal = Math.abs(totalValI).toDouble / 100.0
         val total: BigDecimal = BigDecimal(totalVal)
-        val sum               = List.fill(10)(rate).fold(0.0)(_ + _)
-        val prod              = rate * 10
-
+        val sum = List.fill(10)(rate).fold(0.0)(_ + _)
+        val prod = rate * 10
         val sut = fixture(PensionTaxRate(sum / 100), StatePensionGross -> Amount.gbp(total, StatePensionGross.apiValue))
-        sut.calculation.includePensionIncomeForRate(Rate(prod)).roundAmount() mustBe Amount
+        sut.calculation.includePensionTaxForRate(Rate(prod)).roundAmount() mustBe Amount
           .gbp(total, s"$total(itfStatePensionLsGrossAmt)")
           .roundAmount()
       }
