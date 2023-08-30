f822f7727a11b412421a3573e62a65e29f7e30cd
SandeepUmredkar <sandeep.umredkar@digital.hmrc.gov.uk>
2023-08-30 11:02:57 +0100

    DDCNL-7625

--- test/transformers/ATSCalculationsTest.scala
+++ test/transformers/ATSCalculationsTest.scala
@@ -257,7 +257,7 @@ class ATSCalculationsTest extends BaseSpec with ScalaCheckPropertyChecks with Do
         val sum = List.fill(10)(rate).fold(0.0)(_ + _)
         val prod = rate * 10
         val sut = fixture(PensionTaxRate(sum / 100), StatePensionGross -> Amount.gbp(total, StatePensionGross.apiValue))
-        sut.calculation.includePensionTaxForRate(Rate(prod)).roundAmount() mustBe Amount
+        sut.calculation.includePensionIncomeForRate(Rate(prod)).roundAmount() mustBe Amount
           .gbp(total, s"$total(itfStatePensionLsGrossAmt)")
           .roundAmount()
       }
