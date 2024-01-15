3465fbae6b796f9f7a41744524cfc79207153c8f
grantrobson <grant.robson@digital.hmrc.gov.uk>
2024-01-15 10:58:09 +0000

    8197 Remove pertax auth action

--- app/controllers/auth/PayeAuthAction.scala
+++ app/controllers/auth/PayeAuthAction.scala
@@ -17,11 +17,13 @@
 package controllers.auth
 
 import com.google.inject.{ImplementedBy, Inject}
+import connectors.PertaxConnector
 import models.PertaxApiResponse
 import models.admin.PertaxBackendToggle
-import play.api.Logger
 import play.api.mvc.Results._
 import play.api.mvc._
+import play.api.{Logger, mvc}
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisationException, AuthorisedFunctions, ConfidenceLevel}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.mongoFeatureToggles.services.FeatureFlagService
@@ -32,48 +34,54 @@ import scala.concurrent.{ExecutionContext, Future}
 class PayeAuthActionImpl @Inject() (
   val authConnector: AuthConnector,
   cc: ControllerComponents,
-  featureFlagService: FeatureFlagService
+  featureFlagService: FeatureFlagService,
+  pertaxConnector: PertaxConnector
 )(implicit ec: ExecutionContext)
     extends PayeAuthAction
     with AuthorisedFunctions {
 
   private val logger = Logger(getClass.getName)
 
+  private def retrieveNinoFromAuthClient(implicit hc: HeaderCarrier): Future[Option[String]] =
+    authorised(ConfidenceLevel.L50).retrieve(Retrievals.nino) {
+      case None        => Future.successful(None)
+      case n @ Some(_) => Future.successful(n)
+    }
+
+  private def authenticateViaPertax(
+    ninoOpt: Option[String]
+  )(implicit hc: HeaderCarrier): Future[Option[mvc.Results.Status]] =
+    featureFlagService.get(PertaxBackendToggle).flatMap { toggle =>
+      if (toggle.isEnabled) {
+        ninoOpt match {
+          case None       =>
+            println("\n\n** NO NINO FOUND IN AUTH CLIENT")
+            Future.successful(Some(Unauthorized))
+          case Some(nino) =>
+            println("\nNINO FOUND IN AUTH CLIENT:" + nino)
+            pertaxConnector.pertaxAuth(nino).value.flatMap {
+              case Right(PertaxApiResponse("ACCESS_GRANTED", _, _, _))       =>
+                println("\nGRANTED")
+                Future.successful(None)
+              case Right(PertaxApiResponse("INVALID_AFFINITY", _, _, _))     =>
+                Future.successful(None)
+              case Right(PertaxApiResponse("NO_HMRC_PT_ENROLMENT", _, _, _)) =>
+                Future.successful(Some(Unauthorized))
+              case e                                                         =>
+                Future.successful(Some(InternalServerError))
+            }
+        }
+      } else {
+        Future.successful(None)
+      }
+    }
+
   override protected def filter[A](request: Request[A]): Future[Option[Result]] = {
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
-    authorised(ConfidenceLevel.L50) {
-
-
-
-
-
-//      featureFlagService.get(PertaxBackendToggle).flatMap { toggle =>
-//        if (toggle.isEnabled) {
-//          nino match {
-//            case Some(ninoVal) =>
-//              pertaxConnector.pertaxAuth(ninoVal).value.flatMap {
-//                case Right(PertaxApiResponse("ACCESS_GRANTED", _, _, _))       =>
-//                  Future.successful(None)
-//                case Right(PertaxApiResponse("INVALID_AFFINITY", _, _, _))     =>
-//                  Future.successful(None)
-//                case Right(PertaxApiResponse("NO_HMRC_PT_ENROLMENT", _, _, _)) =>
-//                  Future.successful(Some(Unauthorized))
-//                case _                                                         =>
-//                  Future.successful(Some(InternalServerError))
-//              }
-//            case None          => Future.successful(None)
-//          }
-//        } else {
-//          Future.successful(None)
-//        }
-//      }
-      
-      
-      
-      
-      
-      Future.successful(None)
-    }
+    for {
+      optNino   <- retrieveNinoFromAuthClient
+      optStatus <- authenticateViaPertax(optNino)
+    } yield optStatus
   }.recover {
     case ae: AuthorisationException =>
       logger.error(s"Authorisation exception", ae)
